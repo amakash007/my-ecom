a3744041b70e41a786978cf778297bf2dda1a627
Akashdhage <dhageakash2004@gmail.com>
2025-08-08 20:46:42 +0530

    add url

--- src/components/Products/FilterSidebar.jsx
+++ src/components/Products/FilterSidebar.jsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from 'react'
-import { useSearchParams } from 'react-router-dom'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 
 const FilterSidebar = () => {
 
 const [searchParams,setSearchParams] = useSearchParams();
+const navigate = useNavigate();
 const [filters, setFilters] = useState({
 
     category:"",
     gender:"",
     color:"",
     size:[],
-    meterial:[],
+    material:[],
     brand:[],
     minPrice:0,
     maxPrice:100,
@@ -86,12 +87,21 @@ const sizes = ["XS","S","M","L","XL","XXL"];
                 newFilters[name] = value;
             }
             setFilters(newFilters);
+            updateURLParams(newFilters);
             // console.log(newFilters);
             };
 
             const updateURLParams = (newFilters) =>{
                 const params = new URLSearchParams();
-                
+                Object.keys(newFilters).forEach ((key) =>{
+                    if(Array.isArray(newFilters[key]) && newFilters[key].length > 0){
+                        params.append(key,newFilters[key].join(","));
+                    } else if (newFilters[key]){
+                         params.append(key,newFilters[key]);
+                    } ;
+                });
+                setSearchParams(params);
+                navigate(`?${params.toString()}`); // ?categoty=bottom+wear&size=XS%2CS
             }
 
   return (  
@@ -106,6 +116,7 @@ const sizes = ["XS","S","M","L","XL","XXL"];
             <input type="radio" name="category"
                 value={category}
                 onChange={handleFilterChange}
+                checked={filters.category === category}
             className='mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300' />
          <span className="text-gray-700  ">{category}</span>
         </div>
@@ -121,6 +132,7 @@ const sizes = ["XS","S","M","L","XL","XXL"];
             <input type="radio" name="gender"
             value={gender}
              onChange={handleFilterChange}
+             checked={filters.gender === gender}
              className='mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300' />
          <span className="text-gray-700  ">{gender}</span>
         </div>
@@ -135,8 +147,8 @@ const sizes = ["XS","S","M","L","XL","XXL"];
                  <button key={color} name='color' 
                  value={color}
                  onClick={handleFilterChange}
-                 className='w-8 h-8   rounded-full border border-gray-300 cursor-pointer transtion
-                    hover:scale-105 '
+                 className={`w-8 h-8   rounded-full border border-gray-300 cursor-pointer transtion
+                    hover:scale-105 ${filters.color === color ? "ring-2 ring-blue-500" : ""} `}
                     style={{backgroundColor:color.toLowerCase()}}
                     ></button>
                 ))}
@@ -153,6 +165,7 @@ const sizes = ["XS","S","M","L","XL","XXL"];
                         <input type="checkbox" name="size" 
                         value={size}
                          onChange={handleFilterChange}
+                         checked={filters.size.includes(size)}
                         className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
                         />
                         <span className='text-gray-700'>{size}</span>
@@ -166,9 +179,10 @@ const sizes = ["XS","S","M","L","XL","XXL"];
                 <label className='block text-gray-600 font-medium mb-2 '>Material</label>  
                 {sizes.map((material) => (
                     <div key={material} className='flex items-center mb-1 '>
-                        <input type="checkbox" name="material" 
+                        <input type="checkbox" name="material"
                         value={material}
                          onChange={handleFilterChange}
+                         checked={filters.material.includes(material)}
                         className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
                         />
                         <span className='text-gray-700'>{material}</span>
@@ -180,11 +194,12 @@ const sizes = ["XS","S","M","L","XL","XXL"];
           {/* brand filter */}
         <div className="mb-6">
              <label className='block text-gray-600 font-medium mb-2 '>Brand</label>  
-             {sizes.map((brand) => (
+             {brands.map((brand) => (
                 <div key={brand} className='flex items-center mb-1 '>
                     <input type="checkbox" name="brand" 
-                    value={brand}
+                    value={brand}   
                      onChange={handleFilterChange}  
+                       checked={filters.brand.includes(brand)}
                     className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
                     />
                     <span className='text-gray-700'>{brand}</span>
