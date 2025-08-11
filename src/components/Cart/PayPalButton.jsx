c593fb5e38919d394866d01c43ecc475e9b2f644
Akashdhage <dhageakash2004@gmail.com>
2025-08-11 22:23:32 +0530

    add .env file

--- src/components/Cart/PayPalButton.jsx
+++ src/components/Cart/PayPalButton.jsx
@@ -1,3 +1,30 @@
+  // import React from "react";
+  // import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+
+  // const PayPalCheckout = ({ amount, onSuccess, onError }) => {
+  //   return (
+  //     <PayPalScriptProvider
+  //       options={{
+  //         "client-id": import.meta.env,VITE_PAYPAL_CLIENT_ID
+  //       }}
+  //     >
+  //       <PayPalButtons
+  //         style={{ layout: "vertical" }}
+  //         createOrder={(data, actions) => {
+  //           return actions.order.create({
+  //             purchase_units: [{ amount: { value: amount } }],
+  //           });
+  //         }}
+  //         onApprove={(data, actions) => {
+  //           return actions.order.capture().then(onSuccess);
+  //         }}
+  //         onError={onError}
+  //       />
+  //     </PayPalScriptProvider>
+  //   );
+  // };
+
+  // export default PayPalCheckout;
 import React from "react";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
@@ -5,7 +32,7 @@ const PayPalCheckout = ({ amount, onSuccess, onError }) => {
   return (
     <PayPalScriptProvider
       options={{
-        "client-id": "AUe2VPK7k_RVVyGvgqk0y4Nsw4J4IO3yh9zDk7zoRDtvVysGzsp4mUU_b5qwXo6PN96_8ih_eodABcV0"
+        "client-id": import.meta.env.VITE_PAYPAL_CLIENT_ID 
       }}
     >
       <PayPalButtons
--- src/pages/MyOrderPage.jsx
+++ src/pages/MyOrderPage.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const MyOrderPage = () => {
 
 const[orders, setOrders] = useState([]);
-
+const navigate = useNavigate();
 useEffect(() => {
     // fething order
     setTimeout(() => {
@@ -41,6 +42,10 @@ useEffect(() => {
     }  , 1000 );
 }, []);
 
+const handleRowClick =(orderId) =>{
+    navigate(`/order/${orderId}`);
+};
+
   return (<div className='max-w-7xl mx-auto p-4 sm:p-6'>
         <h2 className='text-xl sm:text-2xl font-bold mb-6'>My Orders</h2>
         <div className="relative shadow-md sm:rounded-lg overflow-hidden">
@@ -59,7 +64,10 @@ useEffect(() => {
                 <tbody>
                     {orders.length > 0 ? (
                         orders.map((order) => (
-                         <tr key={order._id} className='border-b hover:border-gray-50 cursor-pointer'>
+                         <tr 
+                            key={order._id} 
+                            onClick={() => handleRowClick(order._id)}
+                            className='border-b hover:border-gray-50 cursor-pointer'>
                             <td className='py-2 px-4 sm:py-4 sm:px-4'>
                                 <img src={order.orderItem[0].image} 
                                 alt={order.orderItem[0].name}
