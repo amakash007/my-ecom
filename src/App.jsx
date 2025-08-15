a80ad07a60d2fbf6e9408915cf83ef49e50b826d
Akashdhage <dhageakash2004@gmail.com>
2025-08-15 15:35:46 +0530

    complte frontend

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import AdminHomePage from './pages/AdminHomePage'
 import UserManagment from './components/Admin/UserManagment'
 import ProductManagement from './components/Admin/ProductManagement'
 import EditProductPage from './components/Admin/EditProductPage'
+import OrderManagement from './components/Admin/OrderManagement'
 
 
 const App = () => {
@@ -43,6 +44,8 @@ const App = () => {
         <Route path="users" element={<UserManagment/>} /> 
          <Route path="products" element={<ProductManagement/>} /> 
         <Route path="products/:id/edit" element={<EditProductPage/>} /> 
+        <Route path='orders' element={<OrderManagement/>} />
+
         </Route> 
     </Routes>
 
